a14be578d109a12f358a9e2db5b47b757d546ffe
pmeszaros <patrik.meszaros@wup.hu>
2021-09-09 15:30:19 +0200

    Added JUnit and a couple of unit tests

--- src/main/java/com/example/demo/service/WeatherService.java
+++ src/main/java/com/example/demo/service/WeatherService.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+
+    @LocalServerPort
+    int randomServerPort;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testApiResponseIsOk() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        final String url = "http://localhost:" + randomServerPort + "/current-weather";
+
+        URI uri = new URI(url);
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        Assertions.assertEquals(200, result.getStatusCodeValue());
+    }
+
+    @Test
+    public void testApiResponseIsCorrect() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        final String url = "http://localhost:" + randomServerPort + "/current-weather";
+
+        URI uri = new URI(url);
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        Assertions.assertEquals(200, result.getStatusCodeValue());
+        Assertions.assertTrue(Objects.requireNonNull(result.getBody()).contains("Budapest"));
     }
 
 }
